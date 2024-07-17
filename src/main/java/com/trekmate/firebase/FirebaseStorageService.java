8db37f1440a76070db3f180fc70ead872e4fc6d4
Rajeev Singh <rsinghh68@gmail.com>
2024-07-17 08:54:55 +0530

    Acl is now set through code to make the file publicaly accessible for red

--- src/main/java/com/trekmate/firebase/FirebaseStorageService.java
+++ src/main/java/com/trekmate/firebase/FirebaseStorageService.java
@@ -1,6 +1,7 @@
 package com.trekmate.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Acl;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
@@ -12,6 +13,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -41,7 +44,10 @@ public class FirebaseStorageService {
         String blobString = "images/" + UUID.randomUUID() + "-" + file.getName();
 
         BlobId blobId = BlobId.of(bucketName, blobString);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+            .setContentType("image/" + file.getName().substring(file.getName().lastIndexOf(".") + 1))
+            .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+            .build();
         storage.create(blobInfo, Files.readAllBytes(file.toPath()));
 
         return String.format("https://storage.googleapis.com/%s/%s", bucketName, blobString);
